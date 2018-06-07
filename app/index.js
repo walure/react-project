f89b66d38cb4f5c10c8f16c9d0b79cb61c0f7cc0
lu.wang <158322321@qq.com>
2018-06-07 09:59:38 +0800

    1

---
+++ app/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom' 
+
+ReactDOM.render(
+  <div>你好</div>,
+  document.getElementById('app')
+)
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+  entry:'./app/index.js',
+  output:{
+    filename:'index.js',
+    path:path.resolve(__dirname,'dist'),
+    publicPath:'temp/'//动态更新首页地址
+  },
+  devServer:{
+    contentBase:'./',//设置目录,
+    host:'localhost',//访问地址
+    compress:true,
+    port:3000
+  },
+  module:{
+    rules:[
+        {
+            test:/\.js$/,
+            exclude:/node_modules/,
+            loader:"babel-loader",
+            query:{
+                presets:['es2015','react']
+            }
+        }
+    ]
+  }
+}
